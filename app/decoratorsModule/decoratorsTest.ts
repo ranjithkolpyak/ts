1f29b07489362564a959326483bc22329aa1a97e
Ranjith Kolpyak <ranjithkolpyak@Ranjiths-MacBook-Pro.local>
2019-04-21 22:24:09 -0500

    course end

--- app/decoratorsModule/decoratorsTest.ts
+++ app/decoratorsModule/decoratorsTest.ts
@@ -4,7 +4,7 @@ function logger(constructorFn: Function) {
     console.log(constructorFn);
 }
 
-@logger
+// @logger
 class Employee {
     constructor() {
         console.log('Hello World!');
@@ -14,10 +14,77 @@ class Employee {
 //Factory
 
 function logging(value: boolean) {
+    //you can put null for else case but the compiler throws error but you can still run though
     return value ? logger : null;
 }
 
-@logging(true)
+// @logging(true)
 class Car {
-
+    constructor() {
+        console.log('Logging');
+    }
 }
+
+//advanced decorators
+function printable(constructorFn: Function) {
+    constructorFn.prototype.print = function() {
+        console.log(this);
+    }
+}
+
+// @logging(true)
+@printable
+class Plant {
+    name: string = "Green Plant";
+}
+
+// const plant = new Plant();
+// (<any>plant).print();
+
+//method decorators
+
+function editable(value: boolean) {
+    return function(target: any, propName: string, descriptor: PropertyDescriptor) {
+        // console.log(target);
+        // console.log(propName);
+        descriptor.writable = value;
+    }
+}
+
+//Property Decorators
+function overwritable(value: boolean) {
+    return function(target: any, propName:string) : any{
+        const newDescriptor: PropertyDescriptor = {
+            writable: value
+        }
+        return newDescriptor;
+    }
+}
+
+//parameter decorators
+function printInfo(target: any, methodName: string, paramIndex: number){
+    console.log(target, methodName, paramIndex);
+}
+
+class Project {
+    // @overwritable(false)
+    public name: string = "TS";
+
+    constructor(@printInfo name:string) {
+        this.name = name;
+    }
+
+    @editable(false)
+    calcBudget(mode: string, @printInfo flag:boolean) {
+        console.log(10000);
+    }
+}
+
+const project = new Project("Typescript");
+// project.calcBudget();
+// project.calcBudget = function() {
+//     console.log(100);
+// }
+// project.calcBudget();
+// console.log(project);
+
--- index.ts
+++ index.ts
@@ -2,3 +2,13 @@ import { PI } from './app/decoratorsModule/decoratorsTest';
 
 console.log(PI);
 // console.log(genericEcho({name: "Ranjith", age: 27}));
+
+//using jQuery with typescript 
+
+//to resolve the error with $ as undefined you can declare it as a var
+// declare var $:any;
+//this will remove the compiler error
+//instead of this aaproach, you can define or use type defintions of other libraries 
+//tsc will use these type definitions to compile the library specific functions
+
+// $('button').click(function() { alert('Button Clicked!')})
